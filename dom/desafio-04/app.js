7a2ecb8a93f470d052117643a55f4996fa6f73cd
Robson Garola <robsongarola@hotmail.com>
2022-03-19 10:41:35 -0300

    19-03

--- dom/desafio-04/app.js
+++ dom/desafio-04/app.js
@@ -1,14 +1,32 @@
 new Vue({
 	el: '#desafio',
 	data: {
+		classe1:'destaque',
+		minhaClasse:'',
+		perigo:'true'
 
 	},
 	methods: {
 		iniciarEfeito() {
 
+			setInterval(()=>{
+				this.classe1= this.classe1=='destaque'? 'encolher':'destaque'
+			},1000)
+
 		},
 		iniciarProgresso() {
 
+		},
+
+		getPerigo(event){
+
+			if(event.target.value=='true'){
+
+				this.perigo=true
+			}else if(event.target.value=='false'){
+
+				this.perigo=false
+			}			
 		}
 	}
 })
